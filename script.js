95bf68834d511949ac76f16b013f30c740f2bb00
Eduardo Angelo <74938528+jeduardoangelo@users.noreply.github.com>
2021-10-18 14:56:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var nome = "Eduardo"
+
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 7
+var notaDoTerceiroBimestre = 4
+var notaDoQuartoBimestre = 2
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(2)
+
+console.log("Bem vindo " + nome)
+//console.log(notaDoPrimeiroBimestre)
+console.log(notaFixada)
+
+
+
+//Isso é um comentário
